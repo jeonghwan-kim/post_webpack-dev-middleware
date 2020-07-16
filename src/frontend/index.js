313c18e83f2327d5b353174fb6c7ec990e05a0ff
김정환 <jh.kim@woowahan.com>
2020-07-16 22:37:05 +0900

    init

---
+++ src/frontend/index.js
@@ -0,0 +1,5 @@
+function onload() {
+	document.getElementById('app').innerHTML = 'Hello world!'
+}
+
+document.addEventListener('DOMContentLoaded', onload);
---
+++ src/index.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+const path = require('path')
+const middleware = require('webpack-dev-middleware');
+const compiler = webpack(require('../webpack.config'));
+const express = require('express');
+const device = require('express-device');	
+const app = express();
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+if (process.env.NODE_ENV === 'development') {
+	// 웹팩 설정 
+	app.use(
+		'/dist',
+		middleware(compiler, {
+			// webpack-dev-middleware options
+			watch: true
+		})
+	);
+} 
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.resolve(__dirname, '../public')))
+}
+
+app.use(device.capture());
+
+app.get('*', (req, res) => {
+	if (req.device.type === 'desktop') {
+		res.sendFile(path.resolve(__dirname, `../public/desktop.html`))
+		return;
+	} 
+
+	// 모바일 버전 
+	res.sendFile(path.resolve(__dirname, '../public/mobile.html'))
+})
+
+module.exports = app;
